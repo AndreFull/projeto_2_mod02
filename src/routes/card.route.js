9f48d9a34b3f30195fd37e2a6c49aa13cd7aa743
AndreFull <andrelsaaraujo@gmail.com>
2022-09-01 10:42:11 -0300

    innit commit

--- src/routes/card.route.js
+++ src/routes/card.route.js
@@ -1,37 +1,44 @@
+const route = require('express').Router();
+const cardController = require('../controllers/card.controller');
+
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('../../swagger.json');
 
-route.use('/api-docs', swaggerUi.serve);
-route.get('/api-docs', swaggerUi.setup(swaggerDocument));
-
-const express = require('express');
-const router = express.Router();
-
-
-
-
-const cardController = require('../controllers/card.controller');
 //validações das rotas
 const { validId, validObjectBody } = require('../middlewares/card.middleware');
 
 //Rotas do swagger
-
-
+route.use('/api-docs', swaggerUi.serve);
+route.get('/api-docs', swaggerUi.setup(swaggerDocument));
 
 //Rotas de Cards
-router.get('/all-cards',cardController.findAllCardsController)
-router.get('/one-card/:id', validId, cardController.findByIdCardController)
+route.get('/all-cards',cardController.findAllCardsController)
+route.get('/one-card/:id', validId, cardController.findByIdCardController)
 
-router.post('/create-card', validObjectBody, cardController.createCardController)
+route.post('/create-card', validObjectBody, cardController.createCardController)
 
-router.put('/update-card/:id', validId, validObjectBody, cardController.updateCardController)
+route.put('/update-card/:id', validId, validObjectBody, cardController.updateCardController)
 
-router.delete('/delete-card/:id', validId, validObjectBody, cardController.deleteCardController)
+route.delete('/delete-card/:id', validId, validObjectBody, cardController.deleteCardController)
 
 
 //eportar modulo
 
-module.exports = router
+module.exports = route
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
