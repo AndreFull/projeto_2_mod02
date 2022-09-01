28c7e41a6d4e21ebf3aa58076f963e0b78d44a8a
André Luís da Silva Araújo <83524106+AndreFull@users.noreply.github.com>
2022-09-01 16:02:46 -0300

    Update card.route.js

--- src/routes/card.route.js
+++ src/routes/card.route.js
@@ -1,8 +1,5 @@
-const swaggerUi = require('swagger-ui-express');
-const swaggerDocument = require('../../swagger.json');
 
-router.use('/api-docs', swaggerUi.serve);
-router.get('/api-docs', swaggerUi.setup(swaggerDocument));
+
 
 const express = require('express');
 const router = express.Router();
